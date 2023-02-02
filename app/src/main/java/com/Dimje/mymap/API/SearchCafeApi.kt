eb5ea312475a83463a75fed85052ac163df69b8c
Dimje <mark759@naver.com>
2023-02-03 03:35:30 +0900

    [Refactoring] API통신 인터페이스 수정    call -> response    통신 결과와 데이터를 저장할 sealed class 생성

--- app/src/main/java/com/Dimje/mymap/API/SearchCafeApi.kt
+++ app/src/main/java/com/Dimje/mymap/API/SearchCafeApi.kt
@@ -1,18 +1,28 @@
 package com.Dimje.mymap
 
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface SearchCafeService{
+
     @GET("v2/local/search/keyword.json")
-    fun getSearchCafe(
+    suspend fun getSearchCafe(
             @Header("Authorization") REST_API_KEY: String,
             @Query("x") x: Double,
             @Query("y") y: Double,
             @Query("category_group_code") category_group_code : String,
             @Query("radius") radius : Int,
             @Query("query") query: String
-    ): Call<Cafeinfo>
+    ): Response<Cafeinfo>
+
+    @GET("v2/local/search/category.json")
+    suspend fun getSearchOther(
+        @Header("Authorization") REST_API_KEY: String,
+        @Query("x") x: Double,
+        @Query("y") y: Double,
+        @Query("category_group_code") category_group_code : String,
+        @Query("radius") radius : Int
+    ): Response<Cafeinfo>
 }
