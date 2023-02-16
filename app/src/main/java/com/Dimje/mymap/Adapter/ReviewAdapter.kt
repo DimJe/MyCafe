90e5eb5b2cd9715e3d31efbedef4ab3bbc0c2501
Dimje <mark759@naver.com>
2023-02-16 19:36:05 +0900

    [Refactoring] UI 수정

--- app/src/main/java/com/Dimje/mymap/Adapter/ReviewAdapter.kt
+++ app/src/main/java/com/Dimje/mymap/Adapter/ReviewAdapter.kt
@@ -1,12 +1,15 @@
 package com.Dimje.mymap.Adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.Dimje.mymap.Review
 import com.Dimje.mymap.databinding.LayoutReviewRecyclerItemBinding
 
-class ReviewAdapter(val reviewList: List<Review>) : RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder>(){
+class ReviewAdapter(var reviewList: List<Review>) : RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder>(){
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
         val binding = LayoutReviewRecyclerItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return ReviewViewHolder(binding)
@@ -18,6 +21,12 @@ class ReviewAdapter(val reviewList: List<Review>) : RecyclerView.Adapter<ReviewA
 
     override fun getItemCount() : Int {
         return reviewList.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun submit(list: List<Review>){
+        reviewList = list
+        notifyDataSetChanged()
     }
     inner class ReviewViewHolder(private val binding : LayoutReviewRecyclerItemBinding) : RecyclerView.ViewHolder(binding.root){
 
